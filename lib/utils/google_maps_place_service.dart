c91313703b7eb27a9f40daa74e1fc67480bca4a2
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-25 02:19:50 +0200

    add get place details method

--- lib/utils/google_maps_place_service.dart
+++ lib/utils/google_maps_place_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:route_tracker_app/models/place_autocomplete_model/place_autocomplete_model.dart';
+import 'package:route_tracker_app/models/place_details_model/place_details_model.dart';
 
 class GoogleMapsPlaceService {
 
@@ -24,4 +25,23 @@ class GoogleMapsPlaceService {
       throw Exception();
     }
   }
+
+  Future<PlaceDetailsModel> getPlaceDetails({required String placeId}) async {
+
+    var response = await http.get(Uri.parse('$baseUrl/details/json?key=$apiKey&place_id=$placeId'));
+
+    if (response.statusCode == 200){
+      var data = jsonDecode(response.body)['results'];
+      List<PlaceModel> places = [];
+
+      for (var item in data) {
+        places.add(PlaceModel.fromJson(item));
+      }
+      return PlaceDetailsModel.fromJson(data);
+    } else {
+      throw Exception();
+    }
+  }
+
+
 }
