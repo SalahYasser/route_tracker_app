68597941eae9676fe21ca6b0f6e9b7f768788591
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-27 01:27:06 +0200

    implement fetch routes 2 and create routes body model

--- lib/utils/routes_service.dart
+++ lib/utils/routes_service.dart
@@ -1,18 +1,31 @@
+import 'package:route_tracker_app/models/routes_body_model/routes_body_model.dart';
 import 'package:route_tracker_app/models/routes_model/routes_model.dart';
 
 class RoutesService {
-
-  final String baseUrl = 'https://routes.googleapis.com/directions/v2:computeRoutes';
+  final String baseUrl =
+      'https://routes.googleapis.com/directions/v2:computeRoutes';
 
   final String apiKey = 'AIzaSyCQuBDXocPVuelRzdiVtcLnwUHo-mqA2gE';
 
-  Future<RoutesModel> fetchRoutes() {
+  Future<RoutesModel> fetchRoutes(RoutesBodyModel routesBodyModel) {
     Uri url = Uri.parse(baseUrl);
 
     Map<String, String> headers = {
       'Content-Type': 'application/json',
       'X-Goog-Api-Key': apiKey,
-      'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+      'X-Goog-FieldMask':
+          'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+    };
+
+    Map<String, dynamic> body = {
+      "origin": routesBodyModel.origin,
+      "destination": routesBodyModel.destination,
+      "travelMode": routesBodyModel.travelMode,
+      "routingPreference": routesBodyModel.routingPreference,
+      "computeAlternativeRoutes": routesBodyModel.computeAlternativeRoutes,
+      "routeModifiers": routesBodyModel.routeModifiers,
+      "languageCode": routesBodyModel.languageCode,
+      "units": routesBodyModel.units
     };
   }
 }
