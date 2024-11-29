6302491cdeb09b03a0b7f2a52226a609aab5847d
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-29 17:48:30 +0200

    refactor code part one

--- lib/utils/map_services.dart
+++ lib/utils/map_services.dart
@@ -1,12 +1,28 @@
+import 'package:route_tracker_app/models/place_autocomplete_model/place_autocomplete_model.dart';
 import 'package:route_tracker_app/utils/location_service.dart';
 import 'package:route_tracker_app/utils/place_service.dart';
 import 'package:route_tracker_app/utils/routes_service.dart';
 
 class MapServices {
-
   PlacesService placeService = PlacesService();
 
   LocationService locationService = LocationService();
 
   RoutesService routesService = RoutesService();
+
+  getPredictions(
+      {required String input,
+      required String sessionToken,
+      required List<PlaceModel> places}) async {
+
+    if (input.isNotEmpty) {
+      var result = await placeService.getPredictions(
+          sessionToken: sessionToken, input: input);
+
+      places.clear();
+      places.addAll(result);
+    } else {
+      places.clear();
+    }
+  }
 }
